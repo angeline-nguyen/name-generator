d3ddb9f1134b88320ae41079b7a8f2fbec489fe9
angeline-nguyen <angeline.a.nguyen@gmail.com>
2025-04-19 22:22:21 -0700

    Changed throws to toBe

--- js/main.test.js
+++ js/main.test.js
@@ -2,9 +2,9 @@ const validateFirstInitial = require("./main.js");
 const formatBirthMonth = require("./main.js");
 
 test("Returns error if the first initial is not a letter, else returns uppercase letter", () => {
-    expect(validateFirstInitial.validate(" ")).toThrow("First initial must be a letter");
-    expect(validateFirstInitial.validate(".")).toThrow("First initial must be a letter");
-    expect(validateFirstInitial.validate("1")).toThrow("First initial must be a letter");
+    expect(validateFirstInitial.validate(" ")).toBe("First initial must be a letter");
+    expect(validateFirstInitial.validate(".")).toBe("First initial must be a letter");
+    expect(validateFirstInitial.validate("1")).toBe("First initial must be a letter");
     expect(validateFirstInitial.validate("a")).toBe("A");
     expect(validateFirstInitial.validate("A")).toBe("A");
 });
